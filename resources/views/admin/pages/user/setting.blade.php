4a4d9d72422e6a2e68f87d3be15df71e2a75e149
selfiana <selfianafebrianti11@gmail.com>
2019-02-05 14:37:40 +0700

    tokap

--- resources/views/admin/pages/user/setting.blade.php
+++ resources/views/admin/pages/user/setting.blade.php
@@ -3,4 +3,58 @@
 @section('content')
 <h1>User Setting</h1>
 <hr>
+<div class="row">
+	<div class="col-md-6">
+		<form method="post" action="{{ route('admin.user.setting') }}">
+			<div class="card mb-3">
+				<div class="card-header"><h5>Setting</h5></div>
+				<div class="card-body">
+					{{ csrf_field() }}
+
+					<div class="form-group form-label-group">
+						<label for="iName">Name</label>
+						<input type="text" name="name"
+						class="form-control"
+						value="{{ old('name', $dt->name) }}"
+						id="iName" placeholder="Name" required>
+					</div>
+
+					<div class="form-group form-label-group">
+						<label for="iEmail">Email</label>
+						<input type="text" name="email"
+						class="form-control"
+						value="{{ old('email', $dt->email) }}"
+						id="iEmail" placeholder="Email" required>
+					</div>
+
+					<div class="form-group form-label-group">
+						<label for="iPassword">Password</label>
+						<input type="password" name="password"
+						class="form-control"
+						id="iPassword" placeholder="Password" required>
+						<div class="form-text text-muted">
+							<small>Kosongkan password apabila tidak diubah.</small>
+						</div>
+					</div>
+
+					<div class="form-group form-label-group">
+						<label for="iRePassword">Password</label>
+						<input type="password" name="repassword"
+						class="form-control"
+						id="iRePassword" placeholder="Re Password" required>
+					</div>
+
+				</div>
+				<div class="card-footer">
+					<button type="submit" class="btn btn-primary shadow-sm">Update</button>
+					
+				</div>
+				
+			</div>
+			
+		</form>
+		
+	</div>
+	
+</div>
 @endsection
