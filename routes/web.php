4a5cb6a68ba79524f562befbd80dffdc9f8e8e76
selfiana <selfianafebrianti11@gmail.com>
2019-02-24 10:09:42 +0700

    tokap20

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware'=>['auth']], function(){
-	Route::prefix('admin')->group(function(){
+Route::group(['prefix'=>'admin', 'middleware'=>['auth']], function(){
+
 		Route::get('/', function(){
 			return view('admin.pages.dashboard');
 		})->name('admin.home');
 
+		/* User*/
 		Route::prefix('user')->group(function(){
 			Route::get('/', 'UserController@daftar')->name('admin.user')->middleware('akses.admin');
 			Route::delete('/', 'UserController@delete')->middleware('akses.admin');
@@ -38,7 +39,12 @@ Route::group(['middleware'=>['auth']], function(){
 			Route::get('/setting', 'UserSettingController@form')->name('admin.user.setting');
 			Route::post('/setting', 'UserSettingController@update');
 		});
-	});
+
+		/* Kategori*/
+
+		Route::group(['prefix'=>'kategori','middleware'=>'akses.admin'], function(){
+			Route::get('/', 'KategoriController@daftar')->name('admin.kategori');
+		});
 });
 
 
